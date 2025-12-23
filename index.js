f18e2d56f903ff668c64e108805ef86daac4d71b
Feysel Sied <feiselseid123@gmail.com>
2025-12-23 11:23:52 +0300

    Create the increment button.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 console.log('javascript is connected');
+
+const upCount = document.querySelector('#up-count');
+
+const output = document.querySelector('output');
+
+upCount.addEventListener('click', function() {
+    increase();
+})
+
+
+function increase() {
+    let outputValue = output.value;
+    
+    outputValue++;
+    
+    output.value = outputValue;
+}
+
+
